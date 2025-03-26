51035f09b9179ea7d39ad9a89db00c09bbae55ca
Ivy Chapman <ivychapman10@gmail.com>
2025-03-26 16:09:44 +0000

    23/5/25
    added potion

---
+++ Run Away/Assets/turnsoncanvas.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class turnsoncanvas : MonoBehaviour
+{
+    public GameObject canvas; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvas.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
