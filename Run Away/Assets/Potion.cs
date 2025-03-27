0420aa999a4279dc5a48e1954f98c38005839c96
Ivy Chapman <ivychapman10@gmail.com>
2025-03-27 12:33:01 +0000

    potion progress

--- Run Away/Assets/Potion.cs
+++ Run Away/Assets/Potion.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 public class potion : MonoBehaviour
 {
-    Player = GameObject.FindWithTag("Player");
-    private void Start()
+    public GameObject player;
+    public GameObject speedPotion;
+
+
+
+
+
+     void Start()
     {
-        
+
     }
-      
+
+
     private void OnTriggerEnter(Collider other)
     {
-        Destroy(this.gameObject);
-         
-        Player.speed  =+ 10; 
+       
+        player.GetComponent<PlayerMovement>().walkSpeed = +1000000;
+        Destroy(gameObject);
     }
-}
+
+    private void Update()
+    {
+        Vector3 randonSpawnPosition = new Vector3(Random.Range(1820, 1828), -368, Random.Range(-1049, -1040));
+        float spawnTimer = spawnTimer =+ Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            Instantiate(speedPotion, randonSpawnPosition, Quaternion.identity);
+            Debug.Log("I should have spawned");
+            spawnTimer = 0;
+        }
+    }
+ }
